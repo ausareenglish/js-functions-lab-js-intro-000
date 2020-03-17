e3996ceee490cb8e7c07292e34c17c79cc57da60
Tehuti English <ausareenglish@gmail.com>
2020-03-17 00:30:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// write your code below!
 function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
-}
-
-function happycustomHoli(holiday, name) { 
-  return `Happy ${holiday}, ${name}!`;
-}
-
-function holidayCountdown(
+function happyHolidaysTo(name) 
